376b5dc083ef69eeb4550529d6bf8d42dfcd5e48
Matheus Vinicius Inagaki Moraes <82000566+mtsvi-moraes@users.noreply.github.com>
2021-10-15 15:45:28 -0300

    Updated the crawler

--- Crawler/gartner/gartner/items.py
+++ Crawler/gartner/gartner/items.py
@@ -4,9 +4,16 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import Compose, MapCompose, TakeFirst, Join
+from w3lib.html import remove_tags
+
+def get_all_items(item):
+    return item.get_all()
+
 
 
 class GartnerItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    #article = scrapy.Field(input_processor=MapCompose(remove_tags, get_all_items), output_processor=Join())
+    #date = scrapy.Field(input_processor=MapCompose(remove_tags, get_all_items), output_processor=Join())
--- Crawler/gartner/gartner/spiders/articles.py
+++ Crawler/gartner/gartner/spiders/articles.py
@@ -1,10 +1,4 @@
 import scrapy
-from scrapy.loader import ItemLoader
-from itemloaders.processors import TakeFirst, MapCompose
-
-class ScrapingTestingLoader(ItemLoader):
-    default_input_processor = MapCompose(unicode.strip)
-    default_output_processor = TakeFirst()
 
 class ArticlesSpider(scrapy.Spider):
     name = 'articles'
@@ -12,10 +6,10 @@ class ArticlesSpider(scrapy.Spider):
     start_urls = ['http://www.gartner.com/smarterwithgartner']
 
     def parse(self, response):
-        articles = response.css('.rmg-t56 .h4::text').getall()
-        date = response.css('.text .p-small::text').getall()
+        articles = [item.strip() for item in response.css('.rmg-t56 .h4::text').getall()]
+        date = [item.strip() for item in response.css('.text .p-small::text').getall()]
 
         yield {
-            'Articles': articles,
-            'Date': date
+            'Articles':  articles,
+            'Date':  date
         }
