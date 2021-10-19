dd241f1c770234bb7d1fc43636bd33257775fd56
mtsvi-moraes <82000566+mtsvi-moraes@users.noreply.github.com>
2021-10-19 11:37:44 -0300

    Updated crawler

--- Crawler/gartner/gartner/spiders/articles.py
+++ Crawler/gartner/gartner/spiders/articles.py
@@ -2,18 +2,13 @@ import scrapy
 
 class ArticlesSpider(scrapy.Spider):
     name = 'articles'
-    allowed_domains = ['www.gartner.com/smarterwithgartner']
+    allowed_domains = ['www.gartner.com']
     start_urls = ['http://www.gartner.com/smarterwithgartner']
 
     def parse(self, response):
         articles = [item.strip() for item in response.css('.rmg-t56 .h4::text').getall()]
         date = [item.strip() for item in response.css('.text .p-small::text').getall()]
-        link = response.xpath('//a[contains(@data-elem-attr, "href=url;data-type=tags")]/@href').getall()
-
-
-
-        yield {
-            'Articles':  articles,
-            'Date':  date,
-            'Link': link
-        }
+        link = response.xpath('//a[contains(@data-elem-attr, "href=url;data-type=tags")]')
+        for site in link:
+            website = site.xpath('.//@href').get()
+            yield response.follow(url=website)
