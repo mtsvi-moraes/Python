e1ff216d7b4047e774d34639fddb23b015398bc1
mtsvi-moraes <82000566+mtsvi-moraes@users.noreply.github.com>
2021-10-25 22:06:54 -0300

    Moved the test from a test environment

--- Crawler/gartner/gartner/spiders/articles.py
+++ Crawler/gartner/gartner/spiders/articles.py
@@ -1,29 +1,30 @@
 import scrapy
+from scrapy.exceptions import CloseSpider
+import json
 
-class ArticlesSpider(scrapy.Spider):
-    name = 'articles'
+
+class GartnerFinalSpider(scrapy.Spider):
+    name = 'gartner_final'
+    nPage = 0
+    INCREMENTED_BY = 1
     allowed_domains = ['www.gartner.com']
-    start_urls = ['http://www.gartner.com/smarterwithgartner']
+    start_urls = ['https://www.gartner.com/ngw/syspath-bin/gartner/dynamiccontent?requestType=curated-select-by-tags&designType=list&languageCode=en&showLocalizedContent=true&showGcomDomainContent=&filterCodes=153332823&tagType=emt:page/content-type&resourceType=emt/components/content/globalsite/curateddynamiccontent&currentPagePath=https://www.gartner.com/smarterwithgartner/archive&lastSelectedFilter=&tags=emt%3Apage%2Fcontent-type%2Farticles&excludeFilterTags=emt%3Apage%2Fregion%2Fasia-pacific%2Fjapan%2Cemt%3Apage%2Fregion%2Fasia-pacific%2Fchina%2Cemt%3Apage%2Fregion%2Fasia-pacific&pageSizesList=8']
 
     def parse(self, response):
-        link = response.xpath('//a[contains(@data-elem-attr, "href=url;data-type=tags")]')
-        for site in link:
-            website = site.xpath('.//@href').get()
-            yield response.follow(url=website, callback=self.get_information, meta={'Link': website})
-
-    def get_information(self, response):
-        link = response.request.meta['Link']
-        title = response.xpath('//h1/text()').get()
-        pub = response.xpath('(//p/span[@class="p-small"])[1]')
-        for day in pub:
-            publication_date = day.xpath('.//text()').get()
-        #journalist = response.xpath('//p/span[@class="p-small"])[2]')
-        #for publication in journalist:
-            #contributor = publication.xpath('.//text()').get()
-        
+        if response.status == 500:
+            raise CloseSpider('Last Page')
+        r = json.loads(response.body)
+        info = r.get('data')['announcements'][:8]
+        for titles in info:
             yield {
-                'Title': title,
-                'Date': publication_date,
-                'Link': link
-                #'Contributor': contributor
+                'Title': titles['title'],
+                'Date': titles['date']
             }
+
+            self.nPage += self.INCREMENTED_BY
+
+            yield scrapy.Request(
+                url= f'https://www.gartner.com/ngw/syspath-bin/gartner/dynamiccontent?requestType=curated-select-by-tags&designType=list&languageCode=en&showLocalizedContent=true&showGcomDomainContent=&filterCodes=153332823&tagType=emt:page/content-type&resourceType=emt/components/content/globalsite/curateddynamiccontent&currentPagePath=https://www.gartner.com/smarterwithgartner/archive&lastSelectedFilter=&tags=emt%3Apage%2Fcontent-type%2Farticles&excludeFilterTags=emt%3Apage%2Fregion%2Fasia-pacific%2Fjapan%2Cemt%3Apage%2Fregion%2Fasia-pacific%2Fchina%2Cemt%3Apage%2Fregion%2Fasia-pacific&pageSizesList=8&nPage={self.nPage}',
+                callback= self.parse
+            )
+            
