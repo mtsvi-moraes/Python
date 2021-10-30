752293468b0e647e63e049a8ef5bb5f70ea3349f
mtsvi-moraes <82000566+mtsvi-moraes@users.noreply.github.com>
2021-10-30 00:21:56 -0300

    added ibm crawler

--- Crawler/watson/watson/spiders/ibm.py
+++ Crawler/watson/watson/spiders/ibm.py
@@ -1,40 +1,33 @@
 import scrapy
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
 
 
-class IbmSpider(CrawlSpider):
+class IbmSpider(scrapy.Spider):
     name = 'ibm'
-    allowed_domains = ['www.research.ibm.com']
+    allowed_domains = ['www.newsroom.ibm.com']
+    start_urls = ['https://newsroom.ibm.com/announcements']
+
+    def parse(self, response):
+        archive = response.css('#wd_printable_content .wd_title a')
+        lk = response.xpath("//div[@class='wd_title']/a[@href]")
+        for title, txt in archive, lk:
+            file =  title.css('::text').getall(),
+            link = txt.getall()
+
+            yield response.Request(url=link, callback=self.parse_article, meta={'Article': file})
     
-    def start_requests(self):
-        headers = {
-            'authority': 'in.hotjar.com',
-            'method': 'POST',
-            'path': '/api/v2/client/sites/42920/visit-data?sv=5',
-            'scheme': 'https',
-            'accept': '*/*',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
-            'content-length': '129',
-            'content-type': 'text/plain; charset=UTF-8',
-            'origin': 'https://research.ibm.com',
-            'referer': 'https://research.ibm.com/',
-            'sec-ch-ua': '"Google Chrome";v="95", "Chromium";v="95", ";Not A Brand";v="99"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'cross-site',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36'
-        }
-        yield scrapy.Request('https://research.ibm.com/publications', headers=headers)
+    def parse_article(self, response):
+        article = response.request.meta['Article']
+        day = response.xpath("//div[@class='wd_date']")
+        for d in day:
+            date = d.xpath('.//text()').get()
 
-    rules = (
-        Rule(LinkExtractor(restrict_xpaths=("//h2[@class='PublicationListItem_title__2qvDX']/a")), callback='parse_item', follow=True),
-    )
+            yield {
+                'Article': article,
+                'Date': date
+            }
+        
+        
+        # next_page = response.xpath("//li[@class='wd_page_link wd_page_next']/a[@href]/text()").getall()
 
-    def parse_item(self, response):
-        yield {
-            'Title': response.xpath('//h1/text()')
-        }
+        # while next_page:
+        #     yield scrapy.Request(url=next_page, callback=self.parse)
