782d745943eb242207cf4fb762c40486177f35eb
mtsvi-moraes <82000566+mtsvi-moraes@users.noreply.github.com>
2021-11-01 22:15:36 -0300

    minor changes fixed

--- Crawler/watson/watson/spiders/announcements.py
+++ Crawler/watson/watson/spiders/announcements.py
@@ -2,11 +2,11 @@ import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
-
-class TesteSpider(CrawlSpider):
-    name = 'teste'
+class IbmAnnouncements(CrawlSpider):
+    name = 'ibm_announcements'
     allowed_domains = ['newsroom.ibm.com']
     start_urls = ['https://newsroom.ibm.com/announcements']
+    
 
     rules = (
         Rule(LinkExtractor(restrict_xpaths=("//*[@class='wd_title']/a")), callback='parse_item', follow=True),
@@ -15,7 +15,7 @@ class TesteSpider(CrawlSpider):
 
     def parse_item(self, response):
         yield {
-            'Article': response.xpath("(//h1)[2]/text()").get(),
+            'Title': response.xpath("(//h1)[2]/text()").get(),
             'Date': response.xpath("//div[@class='wd_date']/text()").get(),
             'Link': response.url
         }
