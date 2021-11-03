70f3b5773951ffb6ec8ad87b138a455b3dac4f3a
Matheus Vinicius Inagaki Moraes <82000566+mtsvi-moraes@users.noreply.github.com>
2021-11-03 19:00:06 -0300

    finished accenture scraper

--- Crawler/accenture/accenture/settings.py
+++ Crawler/accenture/accenture/settings.py
@@ -71,11 +71,11 @@ ROBOTSTXT_OBEY = False
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
 #AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
---
+++ Crawler/accenture/accenture/spiders/accenture_newsroom.py
@@ -0,0 +1,45 @@
+import scrapy
+
+
+class AccentureNewsroomSpider(scrapy.Spider):
+    name = 'accenture_newsroom'
+    allowed_domains = ['newsroom.accenture.com']
+    Page = 1
+    INCREMENTED_BY = 1
+    start_urls = ['https://newsroom.accenture.com']
+
+    def parse(self, response):
+        title = response.xpath("//h4[@class='media-heading']")
+        for answer in title:
+            heading = answer.xpath('normalize-space(.//a/text())').get(),
+            link = answer.xpath('.//@href').get()
+
+            yield response.follow(url=link, callback=self.parse_article, meta={'Title': heading})
+            
+            self.Page += self.INCREMENTED_BY
+            yield scrapy.Request(
+                url= f'https://newsroom.accenture.com/?page={self.Page}',
+                callback= self.parse
+            )
+            
+
+    
+    def parse_article(self, response):
+        name = response.request.meta['Title']
+        for day in response.xpath("normalize-space(//div[@id='tek-wrap-centerwell']/article/div[1]/text())"):
+                   
+            date = day.get()
+
+            yield {
+                'Title': name,
+                'Date': date
+            }
+
+
+        # self.Page += self.INCREMENTED_BY
+        # page = response.xpath("//li[@class='next ']/a")
+        # url_base = 'https://newsroom.accenture.com'
+        # for req in page:
+        #     next_page = req.xpath(".//@href").extract_first()
+        #     if next_page:
+        #             yield scrapy.Request(url=url_base+next_page+'l', dont_filter=True, callback=self.parse)
