b1055445f60f6453da9677d9665b6f0bbd5bd666
Matheus Vinicius Inagaki Moraes <82000566+mtsvi-moraes@users.noreply.github.com>
2021-11-04 17:18:33 -0300

    Idented Azure crawler

--- Crawler/microsoft/microsoft/spiders/azure.py
+++ Crawler/microsoft/microsoft/spiders/azure.py
@@ -4,8 +4,12 @@ import scrapy
 class AzureSpider(scrapy.Spider):
     name = 'azure'
     page = 1
-    allowed_domains = ['azure.microsoft.com']
-    start_urls = ['https://azure.microsoft.com/en-us/blog/?page=1']
+    allowed_domains = [
+        'azure.microsoft.com'
+    ]
+    start_urls = [
+        'https://azure.microsoft.com/en-us/blog/?page=1'
+    ]
 
     def parse(self, response):
         for info in response.xpath(
@@ -28,7 +32,9 @@ class AzureSpider(scrapy.Spider):
             )
 
     def parse_microsoft_link(self, response):
-        title = response.request.meta['Article']
+        title = response.request.meta[
+            'Article'
+        ]
         Date = response.xpath(
             "//*[@class='text-body5']/text()").get()
         contributor = response.xpath(
